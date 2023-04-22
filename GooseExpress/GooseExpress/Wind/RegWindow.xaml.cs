76c10f62fada471be3b0b0c8a80b5a3a6946d639
Николай Гупало <nikolajgupalo07@gmail.com>
2023-04-22 13:33:47 +0300

    Mark2

--- GooseExpress/GooseExpress/Wind/RegWindow.xaml.cs
+++ GooseExpress/GooseExpress/Wind/RegWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GooseExpress
+namespace GooseExpress.Wind
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для RegWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegWindow : Window
     {
-        public MainWindow()
+        public RegWindow()
         {
             InitializeComponent();
         }
